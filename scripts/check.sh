d6837712c21c561e826bc6e3f07bca0ebbe452ff
Michel Pavan Macedo <michelpm@gmail.com>
2013-02-22 00:39:55 -0300

    Update check script and st2 configuration
    - Convert check.rb to check.sh, since most it does is shell calls
    - Add recent files to check script
    - Update st2 configuration

---
+++ scripts/check.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+echo No output means no differences.
+
+# Go to dotfiles repository
+DOTFILES=$(dirname $(dirname $(readlink -f $0)))
+
+# Diff Sublime Text 2 Configuration
+diff --exclude "*.last-run" --exclude "Default (*).sublime-keymap" $DOTFILES/st2 ~/.config/sublime-text-2/Packages/User
+
+# Diff zsh configuration
+diff $DOTFILES/zshenv ~/.zshenv
+diff $DOTFILES/zshrc ~/.zshrc
+
+# Diff git configuration
+diff $DOTFILES/gitconfig ~/.gitconfig
+
+# Diff ack configuration
+diff $DOTFILES/ackrc ~/.ackrc
+
+APPS=~/.local/share/applications
+# Diff applications configuration
+diff $DOTFILES/mimeapps.list $APPS/mimeapps.list
+diff $DOTFILES/adt/adt.desktop $APPS/adt.desktop
+diff $DOTFILES/adt/adt.desktop $APPS/adt.desktop
+diff $DOTFILES/adt/adt.xpm ~/adt/eclipse/icon.xpm
