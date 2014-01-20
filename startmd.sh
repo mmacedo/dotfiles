86d2b8afef562211256a2388cbb8bb0c9a9aaec1
Michel Pavan Macedo <michelpm@gmail.com>
2014-01-20 18:53:17 -0200

    Update mono scripts

--- startmd.sh
+++ startmd.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-MONO_PREFIX=/opt/mono
-
-export DYLD_FALLBACK_LIBRARY_PATH=$MONO_PREFIX/lib:$DYLD_LIBRARY_FALLBACK_PATH
-export LD_LIBRARY_PATH=$MONO_PREFIX/lib:$LD_LIBRARY_PATH
-export C_INCLUDE_PATH=$MONO_PREFIX/include
-export ACLOCAL_PATH=$MONO_PREFIX/share/aclocal
-export PKG_CONFIG_PATH=$MONO_PREFIX/lib/pkgconfig
-
-export PATH=$MONO_PREFIX/bin:$PATH
+. $(dirname $0)/mono.sh
 
 monodevelop $*
